6bd8c444697e571a61fccd41fe68b8cf131332c2
Raunak Singh <raunak1497@gmail.com>
2020-05-25 16:30:41 +0530

    Made BMI Calculator my first web application

--- my-express-server/calculator/calculator.js
+++ my-express-server/calculator/calculator.js
@@ -7,15 +7,16 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/",function(req,res){
-    res.sendFile(__dirname + "/index.html");
+    res.sendFile(__dirname + "/bmiCalculator.html");
 });
 
 app.post("/",function(req,res){
     // console.log(req.body);
     var num1 = Number(req.body.num1);
     var num2 = Number(req.body.num2);
-    var result = num1+num2;
-    res.send("The result is " +result);
+    num2=num2/100;
+    var result = num1/(num2*num2);
+    res.send("Your BMI is " +result);
 });
 
 app.listen(3000,function(){
