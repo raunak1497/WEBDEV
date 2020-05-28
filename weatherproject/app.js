833b9a4b7500c806e2711f8aeb7b235abfbf8932
Raunak Singh <raunak1497@gmail.com>
2020-05-28 10:58:04 +0530

    Parsing JSON

--- weatherproject/app.js
+++ weatherproject/app.js
@@ -10,11 +10,27 @@ app.get("/",function(req,res){
 
     const url ="https://api.openweathermap.org/data/2.5/weather?q=London&appid=dab3b16def2fbf038730f4434994681e&units=metric";
     https.get(url,function(response){
-        console.log(response);
+        console.log(response.statusCode);
+
+        response.on("data",function(data){
+            // converting hexadecimal to JSON format 
+            const weatherdata = JSON.parse(data);
+            console.log(weatherdata);
+
+            // converting JSON to string 
+            const object = {
+                name : "raunak",
+                food : "maggi"
+            }
+            console.log(JSON.stringify(object));
+
+            const temp = weatherdata.weather[0].description;
+            console.log(temp); 
+        });
     });
         res.send("hello,world!!");
     });
 
 app.listen(3000,function(){
-    console.log("Server is listening on port 3000");
+    console.log("Server is running on port 3000");
 })
