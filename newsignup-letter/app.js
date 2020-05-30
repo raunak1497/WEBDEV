acea720100bb9cf89e3f9156863693a19a7816b8
Raunak Singh <raunak1497@gmail.com>
2020-05-30 15:28:24 +0530

    Making a Newsletter signup project signup page is up

--- newsignup-letter/app.js
+++ newsignup-letter/app.js
@@ -1,26 +1,24 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-// const request = require("request");
+const request = require("request");
 
-const app = express();
+app = express();
 
-app.use(express.static("static"))
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static("static"));
+app.use(bodyParser.urlencoded({extended : true}));
 
 app.get("/",function(req,res){
-    res.sendFile(__dirname+"/signup.html")
+    res.sendFile(__dirname + "/signup.html");
 })
 
 app.post("/",function(req,res){
-    var firstName = req.body.fname; 
-    var lastName = req.body.lname;
-    var email = req.body.email;
+    const firstName = req.body.fname;
+    const lastName = req.body.lname;
+    const email = req.body.email;
 
-    console.log(firstName);
-    console.log(lastName);
-    console.log(email);
-});
-
+    console.log(firstName, lastName ,email);
+    
+})
 app.listen(3000,function(){
-    console.log("server running at port 3000")
-});
+    console.log("Server is up on port 3000");
+}) 
