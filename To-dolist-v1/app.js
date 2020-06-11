fb18db8a4bef6cb55ad6606edb900cead02eed0f
Raunak Singh <raunak1497@gmail.com>
2020-06-11 21:30:15 +0530

    Using layouts

--- To-dolist-v1/app.js
+++ To-dolist-v1/app.js
@@ -5,6 +5,8 @@ const bodyParser = require("body-parser");
 const app = express();
 var items = ["Buy food","cook food","eat food"];
 var item="";
+let workItems = [];
+
 app.set('view engine','ejs');
 
 app.use(bodyParser.urlencoded({extended : true}));
@@ -16,26 +18,45 @@ app.get("/",function(req,res){
 
     var options = {
         weekday: "long",
-        year: "numeric",
+        // year: "numeric",
         day: "numeric",
         month: "long"
     }
 
     var day = today.toLocaleDateString("en-US",options);
 
-    res.render("list",{kindOfDay : day, newListItem : items });
+    res.render("list",{listTitle : day, newListItem : items });
     
     res.send("hi");
 });
 
 app.post("/", function(req,res) {
-    item = req.body.newItem;
 
-    items.push(item);
-    
-    res.redirect("/",)
+    item = req.body.newItem;
+    if(req.body.list  === "Work"){
+        workItems.push(item);
+        res.redirect("/work")
+    }else{
+        items.push(item);
+        res.redirect("/");
+    }
     console.log(item);
 })
+
+app.get("/work",function(req,res){
+    res.render("list",{listTitle : "Work list", newListItem : workItems});
+})
+
+app.post("/work",function(req,res){
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
+
+});
+
+app.get("/about",function(req,res){
+    res.render("about");
+});
 
 app.listen(3000,function(){
     console.log("Server is live");
